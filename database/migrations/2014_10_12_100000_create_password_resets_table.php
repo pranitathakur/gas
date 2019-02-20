6ceca226f8019dd00cd1c23401316fb6c0cf419b
lendetejas <lendetejas8@gmail.com>
2019-02-20 12:14:59 +0530

    database file change

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -16,6 +16,7 @@ class CreatePasswordResetsTable extends Migration
         Schema::create('password_resets', function (Blueprint $table) {
             $table->string('email')->index();
             $table->string('token');
+			$table->string('Name', 30);
             $table->timestamp('created_at')->nullable();
         });
     }
